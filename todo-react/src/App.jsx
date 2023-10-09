ec8eccfbdb3c795a279f6846103a836e225c0ec6
Lucía García <115835658+luciagarpi@users.noreply.github.com>
2023-10-09 16:06:57 +0200

    first commit

---
+++ todo-react/src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-cover bg-center">
+        <header className="container mx-auto px-4">
+            <h1 className="">TODO</h1>
+            <button>luna</button>
+            <form>
+                <input type="text" placeholder="Create a new todo..." />
+            </form>
+        </header>
+        <main className="container mx-auto px-4">
+
+        </main>
+      
+    </div>
+  );
+}
+
+export default App; 
