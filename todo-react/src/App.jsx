442e9fa48810b8d26c6781dc8e1b6eb2e7940ee9
LuciaAccenture <lucia.a.garcia@accenture.com>
2023-10-09 17:38:05 +0200

    Create sections

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,17 +1,34 @@
 const App = () => {
   return (
-    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-cover bg-center">
-        <header className="container mx-auto px-4">
-            <h1 className="">TODO</h1>
-            <button>Doing some changeS</button>
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain">
+        <header className="container mx-auto px-4 pt-8">
+          <div className="flex justify-between">
+            <h1 className="text-white">TODO</h1>
+            <button>Moon</button>
+          </div>
             <form>
                 <input type="text" placeholder="Create a new todo..." />
             </form>
         </header>
-        <main className="container mx-auto px-4">
 
+        <main className="container mx-auto px-4">
+          <article>
+            <button>circle</button>
+            <p>Complete task to be free 😅</p>
+            <button>delete</button>
+          </article>
+          <section>
+            <span>5 items left</span>
+            <button>Clear Completed</button>
+          </section>
         </main>
-      
+
+        <section className="container mx-auto px-4">
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </section>
+        <p className="text-center">Drag and drop to reorder list</p>
     </div>
   );
 }
